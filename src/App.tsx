66fef21b708f0a535c2e7ff0cd71c27e7bec4a14
tgh59njvyp-ship-it <tgh59njvyp@privaterelay.appleid.com>
2026-08-13 13:52:45 +0900

    さあ

--- src/App.tsx
+++ src/App.tsx
@@ -291,6 +291,7 @@ export default function App() {
 
   const handleUpdateCharacter = (updated: CharacterState) => {
     const finalChar = updateCharacterWithTitleCheck(updated);
+    setCharacter(finalChar);
     saveGameToStorage(finalChar, currentStage, floors, phase);
   };
 
